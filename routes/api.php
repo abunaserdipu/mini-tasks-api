92e71ca787b0a6e4772ad80ca555b991a9037a88
abunaserdipu <abunaserdipu0@gmail.com>
2025-12-05 18:12:50 +0600

    :chart_with_upwards_trend: Defined all the relevant routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,28 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\ProjectController;
+use App\Http\Controllers\API\TaskController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// protected
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    // Projects
+    Route::get('/projects', [ProjectController::class, 'index']);
+    Route::post('/projects', [ProjectController::class, 'store']);
+    Route::get('/projects/{id}', [ProjectController::class, 'show']);
+    Route::put('/projects/{id}', [ProjectController::class, 'update']);
+    Route::delete('/projects/{id}', [ProjectController::class, 'destroy']);
+
+    // Tasks nested under projects
+    Route::get('/projects/{projectId}/tasks', [TaskController::class, 'index']);
+    Route::post('/projects/{projectId}/tasks', [TaskController::class, 'store']);
+    Route::get('/projects/{projectId}/tasks/{id}', [TaskController::class, 'show']);
+    Route::put('/projects/{projectId}/tasks/{id}', [TaskController::class, 'update']);
+    Route::delete('/projects/{projectId}/tasks/{id}', [TaskController::class, 'destroy']);
+});
