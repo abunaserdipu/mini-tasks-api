641c85c450a947158d7b6045ebd8d08075127128
abunaserdipu <abunaserdipu0@gmail.com>
2025-12-05 17:08:45 +0600

    :chart_with_upwards_trend: Defined task model & migration

---
+++ app/Models/Task.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Task extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['project_id', 'title', 'description', 'status', 'due_date'];
+
+    protected $casts = [
+        'due_date' => 'date',
+    ];
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+}
